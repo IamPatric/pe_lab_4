2220d8b667d616361c8fe0312a79bb979e108239
IamPatric <brosafari909@gmail.com>
2020-12-22 00:34:42 +0300

    task 1

---
+++ task_2.1.py
@@ -0,0 +1,14 @@
+
+
+from task_1 import get_files_count
+
+
+
+def main():
+    fp = FileProcessing
+    print(f'Task 1: count files\n{get_files_count("C:/pe_lab_4") = }')
+
+
+    
+if __name__ == '__main__':
+    main()
