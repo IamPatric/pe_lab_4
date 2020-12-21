b9a1f34b8d6505a80d206b15189e0995bf9c3888
IamPatric <75327754+IamPatric@users.noreply.github.com>
2020-12-22 00:50:07 +0300

    Update task_2.1.py

--- task_2.1.py
+++ task_2.1.py
@@ -13,7 +13,7 @@ def main():
     print(f'Task 2.2: age > 22\n{[x for x in data[1:] if int(x[2]) >= 22] = }')
     # 2.3
     print(f'Task 2.3: writing in csv\n{fp.write_csv(data, "newfiles.csv") = }')
-    data.append(['11', 'Яблоков Марк Леонидович', '23', '351066'])
+    data.append(['11', 'Якубов Фарид Ильясович', '26', '351066'])
     fp.write_csv(data, "students_changed.csv")
     print(f'Task 2.4: writing in csv changed data\n{data = }')
     print(f'{fp.make_list_from_file("students_changed.csv") = }')
