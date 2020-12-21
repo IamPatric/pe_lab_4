8adf48c28e9391ba83e3df172982a7aabb7c40c1
IamPatric <brosafari909@gmail.com>
2020-12-22 00:39:49 +0300

    csv writing

--- task_2.1.py
+++ task_2.1.py
@@ -10,6 +10,15 @@ def main():
     print(f'Task 2: making list from file\n{fp.make_list_from_file("students.csv") = }')
     data = fp.make_list_from_file("students.csv")
     print(f'Task 2.1: list sorting by last name\n{list_sorting(data[1:], 1) = }')
+    print(f'Task 2.2: age > 22\n{[x for x in data[1:] if int(x[2]) >= 22] = }')
+    # 2.3
+    print(f'Task 2.3: writing in csv\n{fp.write_csv(data, "newfiles.csv") = }')
+    data.append(['11', 'Яблоков Марк Леонидович', '23', '351066'])
+    fp.write_csv(data, "students_changed.csv")
+    print(f'Task 2.4: writing in csv changed data\n{data = }')
+    print(f'{fp.make_list_from_file("students_changed.csv") = }')
+    print(f'Task 2.5: writing in pickle')
+    
     
 if __name__ == '__main__':
     main()
